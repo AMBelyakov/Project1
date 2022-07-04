99b24eb14d2852fca64a4b0922ce33de32f6f770
AMBelyakov <bartemm@mail.ru>
2022-07-04 15:02:02 +0300

    1st video without Django

--- Hello,World!.py
+++ Hello,World!.py
@@ -1,3 +1,25 @@
 print('Hello,World!')
 name='Artem'
 print('Hello,',name)
+
+name=input('Your name')
+print('Hello,',name)
+
+x=float(input('First number:'))
+y=float(input('Second number:'))
+operation=input('Operation:')
+
+result=None
+
+if operation=='+':
+    result=x+y
+elif operation == '-':
+    result = x - y
+elif operation == '*':
+    result = x * y
+elif operation == '/':
+    result = x / y
+else:
+    print('Unsupported operation')
+if result is not None:
+    print('Result:',result)
