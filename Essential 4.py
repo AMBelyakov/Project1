20cb702fea5fd05e41bd1a16206ae9223a1989cd
AMBelyakov <bartemm@mail.ru>
2022-07-14 01:37:08 +0300

    Essential Lesson 4

--- Essential 4.py
+++ Essential 4.py
@@ -149,6 +149,46 @@ def generator():
 for value in generator():
     print(value)
 
+#10
+import random
+import asyncio
+
+
+async def produce():
+
+    await asyncio.sleep(0.5)
+    data=random.randint(1,100)
+    return data
+
+async def consume():
+    sum=0
+    count=0
+    while True:
+        data=await produce()
+        print('Got data:',data)
+
+        sum+=data
+        count+=1
+
+        print('Sum:',sum)
+        print('Average',sum/count)
+        print()
+
+async def another_task():
+    while True:
+        print()
+        print('Hello from another task')
+        print()
+        await asyncio.sleep(1)
+
+if __name__=='__main__':
+    loop=asyncio.get_event_loop()
+    tasks=[consume(),another_task()]
+    loop.run_until_complete(asyncio.wait(tasks))
+
+
+
+
 
 # 5
 class List:
